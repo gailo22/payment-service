f1cd2aeb3a41fa1e23ca152d0f2b5e931cf369b5
Navin Koomsup <navin.koomsup@gmail.com>
2020-10-22 12:31:32 +0700

    Add name to source constructor
    Add name to source constructor

--- src/main/java/net/truedigital/sample/paymentservice/PaymentController.java
+++ src/main/java/net/truedigital/sample/paymentservice/PaymentController.java
@@ -17,7 +17,7 @@ public class PaymentController {
 
     @GetMapping("/accounts/{id}/sources/{sid}")
     public Source getSource(@PathVariable String sid) {
-        return new Source(sid);
+        return new Source(sid, name);
     }
 }
 
