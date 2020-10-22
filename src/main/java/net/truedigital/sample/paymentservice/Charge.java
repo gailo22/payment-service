d137e303c2e2036a1e078469f2feaf814a6477ad
prakit.eng <prakit.eng@truedigital.com>
2020-10-22 16:25:57 +0700

    Add amount to Charge model

--- src/main/java/net/truedigital/sample/paymentservice/Charge.java
+++ src/main/java/net/truedigital/sample/paymentservice/Charge.java
@@ -1,11 +1,19 @@
 package net.truedigital.sample.paymentservice;
 
+import java.math.BigDecimal;
+
 public class Charge {
 
     private String id;
+    private BigDecimal amount;
 
     public Charge(String id) {
         this.id = id;
+    }
+
+    public Charge(String id, BigDecimal amount) {
+        this.id = id;
+        this.amount = amount;
     }
 
     public String getId() {
@@ -15,4 +23,8 @@ public class Charge {
     public void setId(String id) {
         this.id = id;
     }
+
+    public BigDecimal getAmount() { return amount; }
+
+    public void setAmount(BigDecimal amount) { this.amount = amount; }
 }
